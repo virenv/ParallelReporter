ce4860c74d7bcee3e7a8a3c67eee3e44fcb20235
Jon Austen <austenjt@gmail.com>
2017-01-25 14:18:21 -0800

    Added a .gitignore and removed unnecessary files

---
+++ README.md
@@ -0,0 +1,7 @@
+= ParallelReporter
+
+To run this project, add these 3 .jar files to your classpath:
+extentreports-2.41.2.jar
+freemarker-2.3.23.jar
+testng-6.10.jar
+
--- src/Tests/TestBase.java
+++ src/Tests/TestBase.java
@@ -2,10 +2,12 @@ package Tests;
 
 import java.lang.reflect.Method;
 
-import org.testng.annotations.*;
-
 import Interfaces.ITestReporter;
 import ReporterImplementations.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TestBase {
 	
